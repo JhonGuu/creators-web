28b72470f4ad33ffd496e2596f28829d2861c165
JhonGuu <arandajuanjose17@gmail.com>
2024-11-08 22:02:52 -0300

    incorporamos tailwind para los estilos y terminamos por ahora con los bento grid, queda agregarles informacion y probar

--- src/pages/public/Home/components/Bento/Bento.tsx
+++ src/pages/public/Home/components/Bento/Bento.tsx
@@ -1,15 +1,51 @@
  import {BentoItem }from "../BentoItem/BentoItem"
-import style from "./bento.module.css"
+import SanJuan from "../../../../../assets/images/Sanjuan1.jpg"
  export const Bento = () => {
   return (
-    <section className={style.bento}>
+    <section className="w-full max-w-[1400px] grid grid-cols-10 auto-rows-[35rem] gap-4 mx-auto  p-20">
+      
         <BentoItem 
-        className={style.section1} 
+        className='col-span-10 overflow-hidden md:col-span-4' 
         title='Creators El Cenit de los premios SanJuaninos' 
-        subtitle="filo como organizador"><p>San Juan se presta como la Provincia que da nacimiento a este evento, dandonos todos sus lugares iconicos.</p></BentoItem>
-        <BentoItem className={style.section2} title='La alfombra Roja'/> 
-        <BentoItem className={style.section3} title='La Gala'/>
-        <BentoItem className={style.section4} title='Disfruta del discord de la Comunidad'/>
+        subtitle="filo como organizador"
+        >
+          
+            <img 
+            className="absolute top-0 bottom-0 left-0 w-full h-full duration-1000 ease-in-out bg-center bg-no-repeat bg-cover group-hover:scale-110 transition-scale opacity-90 bg-blend-luminosity -z-10 "  
+            src={SanJuan} 
+            alt="" 
+            />
+        
+          <p className="z-40 text-xl text-white">San Juan se presta como la Provincia que da nacimiento a este evento, dandonos todos sus lugares iconicos.</p>
+        </BentoItem>
+        <BentoItem className='col-span-10 overflow-hidden md:col-span-6' title='La alfombra Roja'>
+          
+          <img 
+              className="absolute top-0 bottom-0 left-0 w-full h-full duration-1000 ease-in-out bg-center bg-no-repeat bg-cover group-hover:scale-110 transition-scale opacity-90 bg-blend-luminosity -z-10 "  
+              src={SanJuan} 
+              alt="" 
+              />
+          
+            <p className="z-40 text-xl text-white">San Juan se presta como la Provincia que da nacimiento a este evento, dandonos todos sus lugares iconicos.</p>
+        </BentoItem>
+        <BentoItem className='col-span-10 overflow-hidden md:col-span-6' title='La Gala'>
+          <img 
+                className="absolute top-0 bottom-0 left-0 w-full h-full duration-1000 ease-in-out bg-center bg-no-repeat bg-cover group-hover:scale-110 transition-scale opacity-90 bg-blend-luminosity -z-10 "  
+                src={SanJuan} 
+                alt="" 
+                />
+            
+              <p className="z-40 text-xl text-white">San Juan se presta como la Provincia que da nacimiento a este evento, dandonos todos sus lugares iconicos.</p>
+        </BentoItem>
+        <BentoItem className='col-span-10 overflow-hidden md:col-span-4' title='Disfruta del discord de la Comunidad'>
+          <img 
+                className="absolute top-0 bottom-0 left-0 w-full h-full duration-1000 ease-in-out bg-center bg-no-repeat bg-cover group-hover:scale-110 transition-scale opacity-90 bg-blend-luminosity -z-10 "  
+                src={SanJuan} 
+                alt="" 
+                />
+            
+              <p className="z-40 text-xl text-white">San Juan se presta como la Provincia que da nacimiento a este evento, dandonos todos sus lugares iconicos.</p>
+        </BentoItem>
     </section>
   )
 }
--- src/pages/public/Home/components/BentoItem/BentoItem.tsx
+++ src/pages/public/Home/components/BentoItem/BentoItem.tsx
@@ -1,4 +1,4 @@
-import style from './bentoItem.module.css'
+
 
 interface BentoItemProps {
     className?: string,
@@ -8,12 +8,14 @@ interface BentoItemProps {
     children?: React.ReactNode,
 
 }
-export const BentoItem = ({title, subtitle, image, className,children}:BentoItemProps) => {
+export const BentoItem = ({title, subtitle, className,children}:BentoItemProps) => {
   return (
-        <article  className={`${style.bentoItem} ${className}`}>
-            <div className={style.content}>
-                <h2 className={style.title}>{title}</h2>
-                {subtitle && <h3 className={style.subtitle}>{subtitle}</h3>}
+        <article  className={`relative rounded-xl backdrop-blur-md border border-black/10 shadow-inner shadow-white/10 group ${className}`}>
+            
+            <div className='relative z-20 flex flex-col justify-end h-full gap-1 p-6 text-lg select-none '>
+                <h2 className='z-40 mb-4 text-3xl font-semibold text-white text-balance'>{title}</h2>
+                {subtitle && <h3 className='z-40 -mt-4 text-2xl font-semibold text-sky-200/80'>{subtitle}</h3>}
+                <div className='absolute inset-0 bg-gradient-to-b from-40% from-transparent via-black/80 to-black/80'></div>
                 {children}
             </div>
             
