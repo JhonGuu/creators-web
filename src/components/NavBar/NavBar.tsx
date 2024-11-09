f388489b7109a1985ed8212e908f2992fb313eed
JhonGuu <arandajuanjose17@gmail.com>
2024-11-08 22:54:31 -0300

    cambio en el style

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -24,13 +24,13 @@ export const NavBar = () => {
       <div className={`md:flex md:items-center space-y-4 md:space-y-0 md:space-x-6 transition-all duration-300 
                       ${isOpen ? 'absolute inset-0 top-0 bg-black w-full z-10 h-full flex flex-col  p-6' : 'hidden'} 
                       md:block`}>
-        <div className='flex flex-col items-center justify-center align-middle my-36'>
+        <div className='flex flex-col items-center justify-center align-middle md:flex-row my-36 md:space-y-0 md:gap-4'>
         <Link to="/info" className="font-bold hover:text-gray-300">Info</Link>
         <Link to="/archivo" className="font-bold hover:text-gray-300">Archivo</Link>
         <Link to="/finalistas" className="font-bold hover:text-gray-300">Finalistas</Link>
         <Link to="/votacion" className="font-bold hover:text-gray-300">Votación</Link>
           </div>                
-        <div className='flex flex-row justify-center p-4 m-4 align-bottom'>
+        <div className='flex flex-row justify-center p-4 m-4 align-bottom md:hidden'>
         <IoLogoInstagram width={100} height={100}/>
         <FaSquareXTwitter />
         </div>
